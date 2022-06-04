b4f5db1736060ae12d56b0020fa6e625c4824491
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-04 16:14:48 +0300

    API start

--- src/index.js
+++ src/index.js
@@ -9,3 +9,23 @@ const refs = {
 const dataHits = [{ id: 53633 }, { id: 576655 }];
 refs.cardContainer.insertAdjacentHTML('beforeend', photoCardTpl(dataHits));
 /////////////////////
+
+
+
+///BackEnd///
+import FilmApiService from './js/ApiService';
+const filmApiService = new FilmApiService();
+
+filmApiService.query = 'Spider';
+
+filmApiService.resetPage();
+
+filmApiService.fetchCards()
+    .then(data => {
+        console.log(data);
+        console.log(data.dataResults);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    .finally(console.log('Fetched'));
