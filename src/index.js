ef2dac6d600d7b45f0209aa19409e5430dbf7c0c
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-04 23:04:38 +0300

    API service

--- src/index.js
+++ src/index.js
@@ -1,31 +1,74 @@
 //Templates
 import photoCardTpl from './templates/film-card.hbs';
 
-
-///Example that templates works
-const refs = {
-    cardContainer: document.querySelector('.gallery'), 
-}
-const dataHits = [{ id: 53633 }, { id: 576655 }];
-refs.cardContainer.insertAdjacentHTML('beforeend', photoCardTpl(dataHits));
-/////////////////////
-
-
-
-///BackEnd///
+///BackEnd API///
 import FilmApiService from './js/ApiService';
 const filmApiService = new FilmApiService();
 
-filmApiService.query = 'Spider';
+///Refs
+import getRefs from './js/getRefs';
+const refs = getRefs();
+/////////////
 
+///API
+//методу fetchCards передаємо:
+///тип запиту: popular, searchQuerry, filmInfo
+//Додаткові параметри
+//popularFilm (time_window=day/week)
+//searchQuerry(query)
+//filmInfo(movie_id)
+//В залежності від першого параметра всередині класу вибиратиметься потрібний конструктор url
 filmApiService.resetPage();
+// 'popularFilm', 'day'
+// 'searchQuerry', 'inter'
+// 'filmInfo', '738229'
 
-filmApiService.fetchCards()
+function onHomePageLoad () {
+    filmApiService.fetchCards('popularFilm', 'day')
     .then(data => {
         console.log(data);
-        console.log(data.dataResults);
+        // console.log(data.dataResults);
     })
     .catch(error => {
         console.log(error);
     })
     .finally(console.log('Fetched'));
+}
+
+onHomePageLoad();
+
+//Кожний fetch огортаємо в функцію
+//onSearch(e) передавтиме 'searchQuerry' і рядок запиту
+//onHomePageLoad передавтиме 'popularFilm' і рядок day або week
+//onFilmDetails передавтиме filmInfo' і рядок з id фільму у форматі '738229'ж
+ 
+
+
+
+
+//hero.js: обробка кліку по кнопці
+// Функція onSearch - колбек
+
+//Функція onSearch - приймає event,
+// перевіряє чи є вміст(повідомлення як ні Notifix) <
+//передає запит інпута в API
+//скидає вміст з карочками
+//скидає облік сторінок
+//запускає fetchCards
+
+//Функція FetchCards
+//звертається до APiService, робить запит
+//викликає renderFilmCards
+
+//Функція renderFilmCards
+//отримує фото, назву, жанр, рік
+//рендерить по шаблону
+
+
+
+
+///Зразок рендеру
+const dataHits = [{ id: 53633 }, { id: 576655 }];
+refs.cardContainer.insertAdjacentHTML('beforeend', photoCardTpl(dataHits));
+/////////////////////
+
--- src/js/ApiService.js
+++ src/js/ApiService.js
@@ -1,55 +1,40 @@
-// import axios from 'axios';
-// console.log('before');
-// const axios = require('axios').default;
-// console.log('after');
-
 const BASE_URL = 'https://api.themoviedb.org/3';
-const QUERY_TYPE = '/search/movie';
-// /trending/all/day
-// /movie/{movie_id}
-// /search/movie
-
 const KEY = '8f517ea939a3e5bbc59bc55f9e33cbf1';
-// const PER_PAGE = 40;
 
 
 export default class FilmApiService{
     constructor(){
-        this.searchQuery = '';
         this.page = 1;
     }
 
-    async fetchCards() {
-        const url = `${BASE_URL}${QUERY_TYPE}?api_key=${KEY}&language=en-US&query=${this.searchQuery}&page=${this.page}`;
+    async fetchCards(fetchType, props) {
+        let url = '';
 
-        console.log(url);
+        if (fetchType === 'searchQuerry') {
+            console.log('fetchType: ', 'searchQuerry');
+               url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&query=${props}&page=${this.page}`;
+        }
+
+        if (fetchType === 'popularFilm') {
+            console.log('fetchType: ', 'popularFilm');
+               url = `${BASE_URL}/trending/movie/${props}?api_key=${KEY}&page=${this.page}`;
+        }
+ 
+        if (fetchType === 'filmInfo') {
+            console.log('fetchType: ', 'filmInfo');
+               url = `${BASE_URL}/movie/${props}?api_key=${KEY}&language=en-US`;
+        }
+
+        // console.log('url:', url);
 
         return await fetch(url)
             .then(response => response.json()).then((data) => {
             
-                console.log(data);
+                // console.log(data);
   
-            this.incrementPage();
-
-            const currentPage = this.page - 1;
-            const dataResults = data.results;
-            const totalResults = data.total_results;
-            const totalPages = data.total_pages;
-        
-            return { dataResults, totalResults, totalPages, currentPage };
-            ;
+                this.incrementPage();
+                return data;            ;
         });
-
-        // return await axios.get(url).then(({ data }) => {
-  
-        //     this.incrementPage();
-
-        //     const currentPage = this.page - 1;
-        //     const dataHits = data.hits;
-        //     const totalHits = data.totalHits;
-        
-        //     return { dataHits, totalHits, PER_PAGE, currentPage };
-        // });
 
     }
 
@@ -61,11 +46,26 @@ export default class FilmApiService{
         this.page = 1;
     }
 
-    get query() {
-        return this.searchQuery;
-    }
-
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
 }
+
+//метод filmApiService.fetchCards приймає два рядки: тип запиту (popular, searchQuerry, filmInfo) і пропси(відповідно day або week, рядок із запитом, id фільму)
+
+//На виході Проміс, який треба перетворювати в json, обробити then і catch
+
+//При кожному запиті номер сторінки буде зростати на 1, тому не забуваємо метод resetPage() при "нових запитах"
+
+
+
+
+
+
+
+
+
+//Axios чомусь видає помилку
+
+//Приклади:
+// filmApiService.fetchCards('searchQuerry', 'i')
+// 'popularFilm', 'day'
+// 'searchQuerry', 'fast'
+// 'filmInfo', '738229'
