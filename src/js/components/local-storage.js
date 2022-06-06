8b8d384777863473c46289deee7c269e696d3b36
Tetiana0310 <tetiana.posmashna@gmail.com>
2022-06-06 18:59:07 +0300

    Start localStorage

--- src/js/components/local-storage.js
+++ src/js/components/local-storage.js
@@ -1,3 +1,26 @@
 export default function localStorage(){
     console.log('HEllo frrm localStorage');
+    fetch('https://api.themoviedb.org/3/trending/all/day?api_key=8f517ea939a3e5bbc59bc55f9e33cbf1')
+        .then((response) => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            // localStorage.setItem("data", JSON.stringify(data));
+            // console.log(localStorage.getItem("data"))
+        })
 }
+// const storage = {
+//     addFilm(data, value) {
+//         const result = JSON.stringify(value)
+//         localStorage.setItem(data, result)
+//               }
+// },
+//     getItem (data) {
+//         try {
+//             const savedFilm = localStorage.getItem(key);
+//             return JSON.parse(savedFilm)
+//         }
+//         catch (error) {
+//             console.log("Error parse")
+//         }
+//           }
