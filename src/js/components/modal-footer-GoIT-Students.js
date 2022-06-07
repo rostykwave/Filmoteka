b5be43b3a8faf424d7ae5086ec3dd3f7f9ffae3c
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-06-08 00:24:45 +0300

    fix-modal and loader and bg pattern

--- src/js/components/modal-footer-GoIT-Students.js
+++ src/js/components/modal-footer-GoIT-Students.js
@@ -1,11 +1,5 @@
-export default function modalFooterGoITStudents(){
-  console.log('HEllo frrm modalFooterGoITStudents');
-}
-
-
 document.querySelector('.footer-team-popup').onclick = function() {
   toggle();
-  
 }
 
 function toggle() {
@@ -23,35 +17,21 @@ function toggle() {
   }
 }
 
-
-
 const animation = anime({
   targets: '.footer-wrap .anime',
-  // delay: function(el, i) { return i * 200 },
-  // scale: function(el, i) { return i * 1 },
-  duration: 500, // Can be inherited
-  // easing: 'easeOutExpo', // Can be inherited
+  duration: 500,
   easing: 'spring',
-  // translateY: function(el, i) { 
-  //   return  anime.random(-10, -20) + 'rem';},
+  translateY: [0, function(el, i) {
+    return el.getAttribute('data-y') + 'px';
+  }],
 
-    translateY: function(el, i) {
-      return el.getAttribute('data-y') + 'rem';
-    },
-
-  // translateX: function(el, i) { 
-  //   return  anime.random(-15, 15) + 'rem';},
   scale: [0, 1.2],
-  autoplay: false,
-  // direction: 'alternate', // Is not inherited
-  // loop: true, // Is not inherited
+  autoplay: true,
   opacity: 1,
-  
-  translateX: function(el) {
+
+  translateX: [0, function(el) {
     return el.getAttribute('data-x');
-  },
-  // borderRadius: function() { return ['50%', anime.random(10, 135) + '%']; },
-  // duration: function() { return anime.random(1200, 1800); },
+  }],
   delay: function() { return anime.random(0, 400); },
 
 
--- src/js/loaderSpinner.js
+++ src/js/loaderSpinner.js
@@ -1,3 +1,60 @@
-export function loaderSpinner() {
-    console.log('loaderSpinner');
-}
+const wrapperEl = document.querySelector('.loader-wrapper');
+    const numberOfEls = 90;
+    const duration = 5000;
+    const delay = duration / numberOfEls;
+
+    let tl = anime.timeline({
+      duration: delay,
+      autoplay: false,
+      complete: function() { tl.restart(); }
+    });
+
+    function createEl(i) {
+      let el = document.createElement('div');
+      const rotate = (360 / numberOfEls) * i;
+      const translateY = -20;
+      const hue = Math.round(360 / numberOfEls * i);
+      el.classList.add('loader-el');
+      el.style.backgroundColor = 'hsl(' + hue + ', 40%, 80%)';
+      el.style.transform = 'rotate(' + rotate + 'deg) translateY(' + translateY + '%)';
+      tl.add({
+        begin: function() {
+          anime({
+            targets: el,
+            backgroundColor: ['hsl(' + hue + ', 40%, 80%)', 'hsl(' + hue + ', 90%, 50%)'],
+            rotate: [rotate + 'deg', rotate + 10 +'deg'],
+            translateY: [translateY + '%', translateY + 10 + '%'],
+            scale: [1, 1.2],
+            easing: 'easeInOutSine',
+            direction: 'alternate',
+            duration: duration * .1
+          });
+        }
+      });
+      wrapperEl.appendChild(el);
+    };
+
+    for (let i = 0; i < numberOfEls; i++) {
+        createEl(i)
+    };
+
+
+    //Запуск лоадера. Вместо этого setTimeout поставить addEventListener кнопки поиска фильмов
+    setTimeout(() => {
+        const elem = document.querySelectorAll('.loader-el');
+        console.log('elem :>> ', elem);
+        elem.forEach((element, i) => {
+            const hue = Math.round(360 / numberOfEls * i);
+            element.style.backgroundColor = 'hsl(' + hue + ', 40%, 80%)'
+            
+        });
+        wrapperEl.classList.remove('loader-hidden')
+        tl.restart();
+        
+    }, 0);
+    
+    //Остановка лоадера. Вместо этого setTimeout поставить на finaly когда отрисовалась разметка.
+    // setTimeout(() => {
+    //     wrapperEl.classList.add('loader-hidden')
+    //     tl.pause();
+    // }, 1700);
