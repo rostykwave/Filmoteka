e7aec622f41c3db214dfdd8dff732f749628132e
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-07 18:51:04 +0300

    arrow-up+

---
+++ src/js/components/arrow-up.js
@@ -0,0 +1,30 @@
+'use strict';
+const sttElem = document.querySelector('.stt'),
+  screanHeight = window.innerHeight,
+  sttScroll = function () {
+    document.addEventListener('scroll', function (t) {
+    //   console.log('screanHeight:>> ', screanHeight);
+    //   console.log(' window.scrollY :>> ', window.scrollY);
+      const scrollValue = screanHeight + window.scrollY - 200;
+    //   console.log('scrollValue :>> ', scrollValue);
+      screanHeight <= scrollValue
+        ? sttElem.classList.add('stt__active')
+        : t.target.scrollingElement.scrollTop <= screanHeight &&
+          (sttElem.classList.remove('stt__active'), (sttElem.style.pointerEvents = 'auto'));
+    });
+  },
+  sttClick = function () {
+    sttElem.addEventListener('click', function () {
+      let t = window.scrollY,
+        e = 0,
+        n = t;
+      sttElem.style.pointerEvents = 'none';
+      requestAnimationFrame(function t() {
+        (n -= 5 * (e += 1)), window.scrollTo(0, n), n > 0 && requestAnimationFrame(t);
+      });
+    });
+  },
+  sttFunc = function () {
+    sttScroll(), sttClick();
+  };
+document.addEventListener('DOMContentLoaded', sttFunc);
