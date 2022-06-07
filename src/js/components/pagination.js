c6e92af5636e72109b0f7930ffef624d92ff502f
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-06-07 16:47:59 +0400

    pagination

--- src/js/components/pagination.js
+++ src/js/components/pagination.js
@@ -2,20 +2,20 @@ import {Pagination} from '../functions/pagination-api'
 
 const allPages = 15;
 const step = 5;
-const galleryEl = document.querySelector('.pagination-wrap');
+const homePaginationEl = document.querySelector('.pagination-wrap');
 
-const homePagePagination = new Pagination('home');
+const homepagePagination = new Pagination('home');
 
-homePagePagination.create({
-  prelink: galleryEl,
+homepagePagination.create({
+  prelink: homePaginationEl,
   totalPages: allPages,
   step: step,
   arrows: true,
   dots: true,
 });
 
-homePagePagination.addPaginationEvent(getMovies);
+homepagePagination.addPaginationEvent(getMovies);
 
 function getMovies() {
-  console.log(`Здесь могла быть ваша функция, страница ${homePagePagination.page}.`);
+  console.log(`Здесь могла быть ваша функция, страница ${homepagePagination.page}.`);
 }
