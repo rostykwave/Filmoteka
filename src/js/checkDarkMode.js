516a49fe4266ad753bf079c64f66b084fcb42815
Olena-Milham <nikimilh@gmail.com>
2022-06-08 17:26:43 +0300

    add dark-theme switch & settings

--- src/js/checkDarkMode.js
+++ src/js/checkDarkMode.js
@@ -1,3 +1,33 @@
-export function checkDarkMode(){
-    console.log('checkDarkMode');
+export function checkDarkMode() {
+  console.log('checkDarkMode');
+  const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  const inputEl = document.querySelector('.theme-switch__toggle');
+  const bodyEl = document.querySelector('body');
+  inputEl.addEventListener('change', onInputChange);
+
+  const THEME_KEY = 'theme-color';
+  const savedTheme = localStorage.getItem(THEME_KEY);
+
+  if (savedTheme) {
+    bodyEl.classList.add(savedTheme);
+    if (bodyEl.classList.contains(Theme.DARK)) {
+      inputEl.checked = true;
+    }
+  } else {
+    bodyEl.classList.add(Theme.LIGHT);
+  }
+  function onInputChange() {
+    if (inputEl.checked) {
+      bodyEl.classList.remove(Theme.LIGHT);
+      bodyEl.classList.add(Theme.DARK);
+    } else {
+      bodyEl.classList.remove(Theme.DARK);
+      bodyEl.classList.add(Theme.LIGHT);
+    }
+    localStorage.setItem(THEME_KEY, bodyEl.classList);
+  }
 }
