5a761c38f4af0548798a620c40d18b61e3f2b268
AlexandrRust <123_00@ukr.net>
2022-06-09 19:16:00 +0300

    SetLocalStorageCard

--- src/js/renderFilmCardModal.js
+++ src/js/renderFilmCardModal.js
@@ -1,7 +1,13 @@
 import { refs } from "./getRefs";
 import modalFilmTpl from '../templates/modal-film-description.hbs';
+import { modalWatchedStorage, modalQueueStorage } from "./components/setLocalStorage";
+
 
 export function renderFilmCardModal(data) {
     refs.filmCardModalWrap.innerHTML = "";
     refs.filmCardModalWrap.insertAdjacentHTML('beforeend', modalFilmTpl(data));
+    const modalBtnWatched = document.querySelector('.watched-btn');
+    const modalBtnQueue = document.querySelector('.queue-btn');
+    modalBtnWatched.addEventListener('click', modalWatchedStorage);
+    modalBtnQueue.addEventListener('click', modalQueueStorage);
 }
