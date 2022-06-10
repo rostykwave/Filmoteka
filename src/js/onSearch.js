fda3c5aeb58a5d47cbda070ba7d8bf539f9d6680
DribDrob <drobushenko@gmail.com>
2022-06-11 00:20:56 +0300

    upd onSearch to pagination

--- src/js/onSearch.js
+++ src/js/onSearch.js
@@ -9,15 +9,13 @@ refs.searchForm.addEventListener('submit', onSearch);
 
 const searchPaginationEl = document.querySelector('.pagination-wrap');
 const searchPagination = new Pagination('search'); 
-let resultTotalPages = 0;
 
 export function onSearch(e) {
   e.preventDefault();
 
-searchPagination.resetPage();
+  searchPagination.resetPage();
 
   const query = e.currentTarget.elements.searchQuery.value.trim();
-  console.log(query);
 
   if(query === ''){
     errNotificationShow('Please, fill the search query and try again.', 3000)
@@ -28,52 +26,35 @@ searchPagination.resetPage();
     errNotificationShow('Please, enter the correct movie name and try again.', 3000)
     return; 
   } 
-console.log(query);
-const page = 1;
-
-  startLoader();
-    
-  fetchFilmsOnSearch({page, query})
-        .then(films => {
-            console.log('searchQuery', films);
-
-            if (films.total_results === 0){
-              errNotificationShow('Search result not successful. Enter the correct movie name and try again.', 5000)
-              return;
-              }
-                resultTotalPages = films.total_pages;
- 
-                searchPagination.create({ 
-                  prelink: searchPaginationEl,   
-                  totalPages: resultTotalPages,
-                });
-                console.log('RENDER');
-                renderFilmGallery(films);
-                searchPagination.addPaginationEvent(getSearchMovies);
-    })
-        .catch(onFetchError)
-        .finally(()=>{
-          stopLoader();
-          console.log('fetch searchQuery done');});
-  }
+  getSearchMovies(query)
+}
  
 function errNotificationShow (message, showTime){
-refs.searchForm.insertAdjacentHTML('beforeend', '<p class="form-alert is-hidden"></p>');
-const formAlert = document.querySelector('.form-alert');
-formAlert.textContent = `${message}`;
-formAlert.classList.toggle('is-hidden');
-setTimeout(() => {
+  refs.searchForm.insertAdjacentHTML('beforeend', '<p class="form-alert is-hidden"></p>');
+  const formAlert = document.querySelector('.form-alert');
+  formAlert.textContent = `${message}`;
   formAlert.classList.toggle('is-hidden');
-}, showTime);
+  setTimeout(() => {
+    formAlert.classList.toggle('is-hidden');
+    }, showTime);
 }
-  function getSearchMovies() {
-    filmApiService.setPage(searchPagination.page)
+
+function getSearchMovies(query) {
     startLoader();
-    filmApiService.fetchCards('searchQuery')
+  
+    fetchFilmsOnSearch({page:searchPagination.page, query})
         .then(films => {
-            stopLoader();
-                console.log('RENDER');
-                renderFilmGallery(films);
+          if (films.total_results === 0){
+            errNotificationShow('Search result not successful. Enter the correct movie name and try again.', 5000)
+            return;
+            }
+          renderFilmGallery(films);
+          searchPagination.create({ 
+            prelink: searchPaginationEl,   
+            totalPages: films.total_pages,
+            currentEvent: getSearchMovies,
+          });
     })
         .catch(onFetchError)
+        .finally(stopLoader)
   }
