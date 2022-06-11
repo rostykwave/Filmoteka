f62ed8db5a6a415978cc3dabf646894589fb51cd
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-11 14:49:41 +0300

    all info saved to render card

--- src/js/components/setLocalStorage.js
+++ src/js/components/setLocalStorage.js
@@ -1,50 +1,27 @@
 import { filmApiService } from '../ApiService';
 import genres from '../../json/genres.json';
 
-const filmAdded = {};
-export const textBtn = {};
+// const filmAdded = {};
+// export const textBtn = {};
 
 export function modalWatchedStorage(e) {
 
-  //Дані фільму беремо з розмітки Модалки без Fetch запросів
-  const filmInfo = document.querySelector('.filmCardModal-container');
-  // console.log(filmInfo);
-  const title = filmInfo.querySelector('.film-title').textContent;
-  const img = filmInfo.querySelector('.modal-poster-img').src.replace('https://image.tmdb.org/t/p/w500', '');//підганяємо посилання на зображення до такого яку дає API
-  const genresName = filmInfo.querySelector('#genres').textContent.split(', ');
-  // console.log(genresName);
-  //Перетворення назв жанрів в ID
-  let genresIDS = [];
-
-  genresName.map(genreName => {
-     const genreID = genres.find(option => option.name === genreName).id;
-    genresIDS.push(genreID);
-  })
-
-  console.log(genresName);
-  console.log(genresIDS);
-
+  //Формуєм новий об'єкт з інфо фільму
+  const filmObject = getAllInfoFromCard();
+  
 //зразок записаного в локал файлу(його структура)
   let watched = {
     results: [],
   }
-
-//Записуємо всі дані фільму в один об'єкт
-  const filmObject = {
-    title: title,
-    genre_ids: genresIDS,
-    poster_path: img,
-    release_date:2020,
-  }
-
   //Дістаєм збережений об'єкт з масивом резудьтатів
   const watchedJSONLocal = localStorage.getItem('watched');
+
+  //Якщо є - перезаписуєм
   if (watchedJSONLocal) {
     console.log('Local storage available');
     watched = JSON.parse(watchedJSONLocal);
   } 
   
-
 //Записуємо в його масив новий об'єкт з фільмом
   watched.results.push(filmObject);
 
@@ -99,4 +76,36 @@ export function modalQueueStorage(e) {
       localStorage.removeItem(`Queue-${filmId}`, JSON.stringify(filmAdded));
     }
   });
+}
+
+function getAllInfoFromCard() {
+  //Дані фільму беремо з розмітки Модалки без Fetch запросів
+  const filmInfo = document.querySelector('.filmCardModal-container');
+  // console.log(filmInfo);
+  const title = filmInfo.querySelector('.film-title').textContent;
+  const img = filmInfo.querySelector('.modal-poster-img').src.replace('https://image.tmdb.org/t/p/w500', '');//підганяємо посилання на зображення до такого яку дає API
+  const releaseDate = filmInfo.querySelector('.movie-release-date-inModal').textContent;
+  const voteAverage = filmInfo.querySelector('.values-list__item-vote').textContent;
+  const genresName = filmInfo.querySelector('#genres').textContent.split(', ');
+  // console.log(genresName);
+  
+  //Перетворення назв жанрів в ID
+  let genresIDS = [];
+  genresName.map(genreName => {
+     const genreID = genres.find(option => option.name === genreName).id;
+    genresIDS.push(genreID);
+  })
+
+  //Записуємо всі дані фільму в один об'єкт
+  const filmObject = {
+    title: title,
+    genre_ids: genresIDS,
+    poster_path: img,
+    release_date: releaseDate,
+    vote_average:voteAverage,
+  }
+
+  console.log('filmObject', filmObject);
+
+  return filmObject;
 }
