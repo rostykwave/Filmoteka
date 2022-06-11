0bbb4faa221785f2ed8a82ac3f9f1b0342ec7c74
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-11 09:27:36 +0300

    API  update

--- src/js/ApiService.js
+++ src/js/ApiService.js
@@ -1,16 +1,24 @@
-//3 окремі функції для fetch популярних, пошуку, розширеної інформації про фільм//
+const BASE_URL = 'https://api.themoviedb.org/3';
+const KEY = '8f517ea939a3e5bbc59bc55f9e33cbf1';
+
+//окремі функції для fetch
+
+//Популярні
 export async function fetchPopularFilms({ page, props = 'day' }) {
     const url = `${BASE_URL}/trending/movie/${props}?api_key=${KEY}&page=${page}`;
     return await fetch(url)
         .then(response => response.json());
 }
 
+//Пошук по запиту в інпут
 export async function fetchFilmsOnSearch({page, query}) {
     const url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`;
     
     return await fetch(url)
         .then(response => response.json());
 }
+
+//Пошук по id , для модалки карточки
 export async function fetchFilmInfo(filmId) {
     const url = `${BASE_URL}/movie/${filmId}?api_key=${KEY}&language=en-US`;
     
@@ -21,75 +29,69 @@ export async function fetchFilmInfo(filmId) {
 
 
 
+/////Застарілий код (буде видалений)
 //метод filmApiService.fetchCards приймає два рядки: тип запиту (popular, searchQuery, filmInfo) і пропси(відповідно day або week, рядок із запитом, id фільму)
 
 //На виході отримуємо Проміс, який треба перетворювати в json, обробити then і catch
 
 //При кожному запиті номер сторінки буде зростати на 1, тому не забуваємо метод resetPage() при "нових запитах"
 
-const BASE_URL = 'https://api.themoviedb.org/3';
-const KEY = '8f517ea939a3e5bbc59bc55f9e33cbf1';
 
+// class FilmApiService{
+//     constructor(){
+//         this.page = 1;
+//         this.props = '';
+//     }
 
-class FilmApiService{
-    constructor(){
-        this.page = 1;
-        this.props = '';
-    }
+//     async fetchCards(fetchType) {
+//         let url = '';
 
-    async fetchCards(fetchType) {
-        let url = '';
+//         if (fetchType === 'searchQuery') {
+//             console.log('fetchType: ', 'searchQuery');
+//                url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&query=${this.props}&page=${this.page}`;
+//         }
 
-        if (fetchType === 'searchQuery') {
-            console.log('fetchType: ', 'searchQuery');
-               url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&query=${this.props}&page=${this.page}`;
-        }
-
-        if (fetchType === 'popularFilm') {
-            console.log('fetchType: ', 'popularFilm');
-               url = `${BASE_URL}/trending/movie/${this.props}?api_key=${KEY}&page=${this.page}`;
-        }
+//         if (fetchType === 'popularFilm') {
+//             console.log('fetchType: ', 'popularFilm');
+//                url = `${BASE_URL}/trending/movie/${this.props}?api_key=${KEY}&page=${this.page}`;
+//         }
  
-        if (fetchType === 'filmInfo') {
-            console.log('fetchType: ', 'filmInfo');
-               url = `${BASE_URL}/movie/${this.props}?api_key=${KEY}&language=en-US`;
-        }
+//         if (fetchType === 'filmInfo') {
+//             console.log('fetchType: ', 'filmInfo');
+//                url = `${BASE_URL}/movie/${this.props}?api_key=${KEY}&language=en-US`;
+//         }
 
-        // console.log('url:', url);
+//         // console.log('url:', url);
 
-        return await fetch(url)
-            .then(response => response.json()).then((data) => {
+//         return await fetch(url)
+//             .then(response => response.json()).then((data) => {
             
-                // console.log(data);
+//                 // console.log(data);
   
-                this.incrementPage();
-                return data;            ;
-        });
+//                 this.incrementPage();
+//                 return data;            ;
+//         });
 
-    }
+//     }
 
-    incrementPage() {
-        this.page += 1;
-    }
+//     incrementPage() {
+//         this.page += 1;
+//     }
 
-    resetPage() {
-        this.page = 1;
-    }
+//     resetPage() {
+//         this.page = 1;
+//     }
 
-    setPage(newPage) {
-      this.page = newPage;  
-    }
+//     setPage(newPage) {
+//       this.page = newPage;  
+//     }
 
-    setProps(newProp) {
-        this.props = newProp;
-    }
-}
+//     setProps(newProp) {
+//         this.props = newProp;
+//     }
+// }
 
-export const filmApiService = new FilmApiService();
-
-
-
-
+// export const filmApiService = new FilmApiService();
 
 
 //Axios чомусь видає помилку
