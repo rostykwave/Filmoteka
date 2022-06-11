983d91aa5cf4896ea2995bd5c90466e58db732a8
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-11 16:26:22 +0300

    poster not available fix on GH-PAges

--- src/js/renderFilmGallery.js
+++ src/js/renderFilmGallery.js
@@ -2,6 +2,7 @@ import  genres  from '../json/genres.json'
 import { refs } from './getRefs';
 import filmCardTpl from '../templates/film-card.hbs';
 import { onFilmCardClick } from './onFilmCardClick';
+import posterPlug from '../images/poster-plug.jpg';
 
 export function renderFilmGallery(data) {
 
@@ -20,9 +21,16 @@ export function renderFilmGallery(data) {
                 genresContent = [genresContent[0], genresContent[1], 'Other'];
             }
             hit.genres = genresContent.join(', ');
-            
+           
+            if (!hit.poster_path) {
+                hit.poster_path = posterPlug;
+            } else {
+                const currentPath = hit.poster_path;
+                hit.poster_path = `https://image.tmdb.org/t/p/w500${currentPath}`
+            }
                 return hit;
         })
+
         refs.filmGallery.innerHTML = '';
         refs.filmGallery.insertAdjacentHTML('beforeend', filmCardTpl(film));
     }
