7ca4e7800a134a92263ca9a9aeec79df88ff50ae
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-06-14 20:51:53 +0400

    bugfix_search_pagination

--- src/js/getFilmsOnSearchQuery.js
+++ src/js/getFilmsOnSearchQuery.js
@@ -1,40 +1,54 @@
-import { renderFilmGallery } from "./renderFilmGallery";
-import { startLoader, stopLoader } from "./loaderSpinner";
-import { onFetchError } from "./onFetchError";
-import { fetchFilmsOnSearch } from "./ApiService";
+import { renderFilmGallery } from './renderFilmGallery';
+import { startLoader, stopLoader } from './loaderSpinner';
+import { onFetchError } from './onFetchError';
+import { fetchFilmsOnSearch } from './ApiService';
 import { Pagination } from './components/pagination-api';
-import {queryCurr, errNotificationShow} from './onSearch';
-import { setConfigState } from "./components/savedPageConfig";
+import { queryCurr, errNotificationShow } from './onSearch';
+import { setConfigState } from './components/savedPageConfig';
 
 const searchPaginationEl = document.querySelector('.pagination-wrap');
-export const searchPagination = new Pagination('search'); 
+const localSavedQuery = JSON.parse(localStorage.getItem('reloadAction'));
+
+let lastQuery = localSavedQuery?.q || '';
 
 export function getSearchFilms(queryCurr) {
-  startLoader();
-  // setConfigState({ funcName: 'getSearchFilms', q: queryCurr });
+  const searchPagination = new Pagination('search');
 
-    fetchFilmsOnSearch({page:searchPagination.page, query:queryCurr})
-        .then(films => {
-          if (films.total_results === 0){
-            errNotificationShow('Search result not successful. Enter the correct movie name and try again.', 5000)
-            return;
-            }
-          renderFilmGallery(films);
-          searchPagination.create({ 
-            prelink: searchPaginationEl,   
-            totalPages: films.total_pages,
-            currentEvent: getSearchMovies,
-          });
+  if (queryCurr !== lastQuery) {
+    searchPagination.resetPage();
+  }
+
+  lastQuery = queryCurr;
+
+  startLoader();
+
+  getSearchMovies()
+    .then(films => {
+      searchPagination.create({
+        prelink: searchPaginationEl,
+        totalPages: films.total_pages,
+        currentEvent: getSearchPerPageMovies,
+      });
     })
-        .catch(onFetchError)
-        .finally(stopLoader)
-  }
-function getSearchMovies() {
+    .catch(onFetchError)
+    .finally(stopLoader);
+
+  function getSearchPerPageMovies() {
     startLoader();
-    fetchFilmsOnSearch({page:searchPagination.page, query:queryCurr})
-      .then(films => {
-        renderFilmGallery(films);
-      })
-      .catch(onFetchError)
-      .finally(stopLoader)
+    getSearchMovies().catch(onFetchError).finally(stopLoader);
   }
+
+  function getSearchMovies() {
+    return fetchFilmsOnSearch({ page: searchPagination.page, query: queryCurr }).then(films => {
+      if (films.total_results === 0) {
+        errNotificationShow(
+          'Search result not successful. Enter the correct movie name and try again.',
+          5000,
+        );
+        return;
+      }
+      renderFilmGallery(films);
+      return films;
+    });
+  }
+}
--- src/js/onSearch.js
+++ src/js/onSearch.js
@@ -1,6 +1,6 @@
-import { refs } from "./getRefs";
-import {getSearchFilms, searchPagination} from "./getFilmsOnSearchQuery";
-import { setConfigState } from "./components/savedPageConfig";
+import { refs } from './getRefs';
+import { getSearchFilms } from './getFilmsOnSearchQuery';
+import { setConfigState } from './components/savedPageConfig';
 
 refs.searchForm.addEventListener('submit', onSearch);
 
@@ -9,32 +9,30 @@ export let queryCurr = '';
 export function onSearch(e) {
   e.preventDefault();
 
-  searchPagination.resetPage();
-
   const query = e.currentTarget.elements.searchQuery.value.trim();
 
-  if(query === ''){
-    errNotificationShow('Please, fill the search query and try again.', 3000)
-    return;   
+  if (query === '') {
+    errNotificationShow('Please, fill the search query and try again.', 3000);
+    return;
   }
   let pattern = /^[a-z0-9-\s]+$/i;
-  if (!pattern.test(query)){
-    errNotificationShow('Please, enter the correct movie name and try again.', 3000)
-    return; 
-  } 
+  if (!pattern.test(query)) {
+    errNotificationShow('Please, enter the correct movie name and try again.', 3000);
+    return;
+  }
   queryCurr = query;
 
   setConfigState({ funcName: 'onSearch', q: queryCurr });
-  
-  getSearchFilms(queryCurr)
+
+  getSearchFilms(queryCurr);
 }
- 
-export function errNotificationShow (message, showTime){
+
+export function errNotificationShow(message, showTime) {
   refs.searchForm.insertAdjacentHTML('beforeend', '<p class="form-alert is-hidden"></p>');
   const formAlert = document.querySelector('.form-alert');
   formAlert.textContent = `${message}`;
   formAlert.classList.toggle('is-hidden');
   setTimeout(() => {
     formAlert.classList.toggle('is-hidden');
-    }, showTime);
+  }, showTime);
 }
